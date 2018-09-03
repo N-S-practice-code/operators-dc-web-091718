8cb51ddfdf2c1571249b18e3c5f5e5dade824c3f
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-03 21:09:03 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
-  return true if speed>=40 && speed<=60
-  false
+  return false if speed>=40 && speed<=60
+  true
 end
 
 
 
 def not_safe?(speed)
-	speed>=40 && speed<=60 ? true : false
+	speed>=40 && speed<=60 ? false : true
 end
